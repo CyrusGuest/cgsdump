2b9ba76bf9cd10090712c210f6c2a021f9eade39
Cyrus Guest <cyrus.t.guest@gmail.com>
2024-08-14 01:51:50 -0400

    polish

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -7,15 +7,15 @@ const Album = ({ image, date, link }) => {
     <div
       onMouseOver={() => setShowView(true)}
       onMouseOut={() => setShowView(false)}
-      className="rounded-lg relative hover:opacity-70 hover:cursor-pointer"
+      className="rounded-lg relative hover:opacity-70 hover:cursor-pointer mb-4"
     >
       <a target="_blank" href={link}>
         <img
           src={image}
           alt="album"
-          className="rounded-lg w-36 max-w-sm md:w-full mx-auto border-2 border-gray-400"
+          className="rounded-lg w-40 max-w-sm md:w-full mx-auto border-2 border-gray-400"
         ></img>
-        <p className="z-10 left-1/4 top-8 md:left-1/3 font-bold text-white text-lg md:text-2xl absolute">
+        <p className="z-10 left-1/2 top-1/4 -translate-x-1/2 -translate-y-1/2 font-bold text-white text-lg md:text-2xl absolute">
           {date}
         </p>
         <p
